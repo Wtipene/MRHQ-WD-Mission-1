0409bda13cdc3ff1eef7f4b1f37b6da63ec80cf8
Wilem Tipene <wilemtipene@gmail.com>
2022-06-01 11:29:59 +1200

    finished off files and created history page for show and tell

--- js.js
+++ js.js
@@ -93,3 +93,75 @@ navBar.addEventListener("mouseover", function( event ) {
     }, 100);
   }, false);
 
+
+
+  //HISTORY PAGE
+
+  function qs(selector, all = false) {
+    return all ? document.querySelectorAll(selector) : document.querySelector(selector);
+  }
+  
+  const sections = qs('.section', true);
+  const timeline = qs('.timeline');
+  const line = qs('.line');
+  line.style.bottom = `calc(100% - 20px)`;
+  let prevScrollY = window.scrollY;
+  let up, down;
+  let full = false;
+  let set = 0;
+  const targetY = window.innerHeight * .8;
+  
+  function scrollHandler(e) {
+    const {
+      scrollY
+    } = window;
+    up = scrollY < prevScrollY;
+    down = !up;
+    const timelineRect = timeline.getBoundingClientRect();
+    const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
+  
+    const dist = targetY - timelineRect.top;
+    console.log(dist);
+  
+    if (down && !full) {
+      set = Math.max(set, dist);
+      line.style.bottom = `calc(100% - ${set}px)`;
+    }
+  
+    if (dist > timeline.offsetHeight + 50 && !full) {
+      full = true;
+      line.style.bottom = `-50px`;
+    }
+  
+    sections.forEach(item => {
+      // console.log(item);
+      const rect = item.getBoundingClientRect(); //     console.log(rect);
+  
+      if (rect.top + item.offsetHeight / 5 < targetY) {
+        item.classList.add('show-me');
+      }
+    }); // console.log(up, down);
+  
+    prevScrollY = window.scrollY;
+  }
+  
+  scrollHandler();
+  line.style.display = 'block';
+  window.addEventListener('scroll', scrollHandler);
+
+
+
+  const assets = [
+    "gallery1.jpg",
+    "gallery2.jpg",
+    "gallery3.jpg",
+    "gallery4.jpg"
+    ]
+    i = 0
+    function nextImg(){
+      
+    i = i+1
+    if(i==assets.length)i=0
+    let x = document.getElementById('gallery');
+    x.src=assets[i]
+    }
